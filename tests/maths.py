2759b8a7e34342d83dcb1cb3b93b2ffc761c4f79
Jon Tyler <k98kurz@gmail.com>
2024-05-16 19:41:46 -0400

    proved that the original construction for the point clock will not work

---
+++ tests/maths.py
@@ -0,0 +1,32 @@
+from hashlib import sha256
+from secrets import token_bytes
+from context import classes, interfaces, misc
+import nacl.bindings
+import unittest
+
+
+class RunMathematicalProofs(unittest.TestCase):
+    """Prove some maths to demonstrate insecurity of constructions."""
+    def test_prove_point_can_be_divided_by_two(self):
+        """Proves that doubling a point is not a one-way function."""
+        seed = token_bytes(32)
+        x = misc.derive_key_from_seed(seed)
+        Y = misc.derive_point_from_scalar(x)
+        Y2 = misc.recursive_add_point(Y, 1)
+        Y_2 = divide_point_by_two(Y2)
+        assert Y == Y_2, f'\n{Y.hex()}\n{Y_2.hex()}'
+
+
+def divide_point_by_two(point: bytes) -> bytes:
+    x = misc.clamp_scalar(misc.H_small(b'one'))
+    scalar1 = nacl.bindings.crypto_core_ed25519_scalar_mul(
+        x,
+        nacl.bindings.crypto_core_ed25519_scalar_invert(x)
+    )
+    scalar2 = nacl.bindings.crypto_core_ed25519_scalar_add(scalar1, scalar1)
+    scalarhalf = nacl.bindings.crypto_core_ed25519_scalar_invert(scalar2)
+    return nacl.bindings.crypto_scalarmult_ed25519_noclamp(scalarhalf, point)
+
+
+if __name__ == '__main__':
+    unittest.main()
